6abc82af758d03055a4c64b6b7cc3ffceba55b1e
bhyunnie <dev.bhyunnie@gmail.com>
2023-10-29 17:24:54 +0900

    :tada: [FEAT] Chap4 정리

---
+++ src/main/kotlin/chapt4_nullSafety/Main.kt
@@ -0,0 +1,18 @@
+package chapt4_nullSafety
+
+fun main () {
+    // val a : String = null; // FIXME : 에러 발생
+    var b : String = "aabbcc";
+    var c : String? = null;
+
+    var a : String? = null;
+    // println(a.length); // FIXME : 에러 발생
+    println(a?.length); // js 와 같은 널 체크 기능
+
+    val d : Int = if (a != null) a.length else 0
+    println(d);
+
+    // 엘비스 연산자
+    val e = a?.length ?: 0 // 좌변이 null 인 경우 우변을 리턴
+    println(e);
+}
---
+++ src/main/kotlin/chapt4_nullSafety/NullSafety.kt
@@ -0,0 +1,17 @@
+package chapt4_nullSafety
+
+fun getNullStr() : String? = null;
+
+fun getLengthIfNotNull(str : String) = str?.length;
+
+fun main () {
+    val nullableStr = getNullStr();
+
+    // val nullableStrLength = nullableStr.length; // FIXME : NPE
+    val nullableStrLength = nullableStr?.length ?: 0;
+    print(nullableStrLength);
+
+    // 단언 연산자
+    val c : String? = null;
+    val d = c!!.length; // null이던 아니던 컴파일러가 null 체크를 안하고 넘어가는 거라 쓰면 안됨
+}
